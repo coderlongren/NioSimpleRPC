90a5eaef66b8bf0866fe16d68b2329d76ef3cd24
rensailong <coderlongren@gmail.com>
2019-05-31 15:27:52 +0800

    Hystrix 命令封装 HelloServiceImpl逻辑

--- src/main/java/com/qunar/nioDemo/service/HelloCommand.java
+++ src/main/java/com/qunar/nioDemo/service/HelloCommand.java
@@ -4,21 +4,39 @@ import com.netflix.hystrix.*;
 
 public class HelloCommand extends HystrixCommand<String> {
     private HelloService service;
+    private String name;
 
+    public HelloCommand(HelloService service, String name) {
+        super(setter());
+        this.service = service;
+        this.name = name;
+    }
 
-    private static class Setter {
+    private static Setter setter(){
+        // service group
         HystrixCommandGroupKey groupKey = HystrixCommandGroupKey.Factory.asKey("Group-name");
+        // service name
         HystrixCommandKey commandKey = HystrixCommandKey.Factory.asKey("hello-service");
+        //threadpool name
         HystrixThreadPoolKey threadPoolKey = HystrixThreadPoolKey.Factory.asKey("hello-thread-name");
-        HystrixThreadPoolProperties threadPoolProperties = HystrixThreadPoolProperties.Setter()
+        // threadpool properties
+        HystrixThreadPoolProperties.Setter threadPoolProperties = HystrixThreadPoolProperties.Setter()
                 .withCoreSize(10)
-                .withMaxQueueSize(10000)
-                .withKeepAliveTimeMinutes(1000)
-                .withQueueSizeRejectionThreshold()
+                .withMaxQueueSize(Integer.MAX_VALUE)
+                .withKeepAliveTimeMinutes(5)
+                .withQueueSizeRejectionThreshold(10000);
+        HystrixCommandProperties.Setter hystrixCommandProperties = HystrixCommandProperties.Setter()
+                .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD);
+
+        return HystrixCommand.Setter.withGroupKey(groupKey)
+                .andCommandKey(commandKey)
+                .andThreadPoolKey(threadPoolKey)
+                .andThreadPoolPropertiesDefaults(threadPoolProperties)
+                .andCommandPropertiesDefaults(hystrixCommandProperties);
     }
 
     @Override
     protected String run() throws Exception {
-        return null;
+        return service.sayHello(name);
     }
 }
